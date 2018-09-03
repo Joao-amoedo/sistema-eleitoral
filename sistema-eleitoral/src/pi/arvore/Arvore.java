601a048a55b8415e97e03181ef413333072a213c
João Pedro Amoêdo <jp.advc@gmail.com>
2018-09-03 16:43:16 -0300

    Terminando os métodos da árvore Binária e adicionando comentários

--- sistema-eleitoral/src/pi/arvore/Arvore.java
+++ sistema-eleitoral/src/pi/arvore/Arvore.java
@@ -3,13 +3,27 @@ package pi.arvore;
 import pi.node.Node;
 
 public interface Arvore {
-	
+	/**
+	 * Metodo público que adiciona elemento na árvore, caso o elemeto seja repetido,
+	 * retorna false
+	 * 
+	 * @return adiciona
+	 * @param elemento
+	 */
 	public boolean add(Integer elemento);
-	
+
+	/**
+	 * Metodo público de remoção
+	 * 
+	 * @return removido
+	 * @param elemento
+	 */
 	public boolean remove(Integer elemento);
 	
+	/**
+	 * Retorna a raiz da árvore
+	 * @return raiz
+	 */
 	public Node getRaiz();
-	
-	public int getAltura();
-	
+
 }
--- sistema-eleitoral/src/pi/arvore/ArvoreBinaria.java
+++ sistema-eleitoral/src/pi/arvore/ArvoreBinaria.java
@@ -22,11 +22,12 @@ public class ArvoreBinaria implements Arvore {
 	}
 
 	/**
-	 * Metodo privado
+	 * Método privado sobrecarregado para adicionar elemento na Árvore de forma
+	 * recursivo
 	 * 
 	 * @param raiz
 	 * @param novo
-	 * @return
+	 * @return adiciona
 	 */
 	private boolean add(Node raiz, Node novo) {
 		Node aux = raiz;
@@ -76,17 +77,188 @@ public class ArvoreBinaria implements Arvore {
 
 	@Override
 	public boolean remove(Integer elemento) {
+		if (this.raiz.getElemento() == elemento) {
+			raiz = verificaTipoRemocao(raiz);
+			return true;
+		} else {
+			remove(raiz, elemento);
+		}
 		return false;
 	}
 
+	private boolean remove(Node raiz, Integer elemento) {
+		if (raiz.getElemento() < elemento) {
+			Node direita = raiz.getDireita();
+			if (direita == null)
+				return false;
+			else if (direita.getElemento() != elemento)
+				return remove(direita, elemento);
+			else {
+				raiz.setDireita(verificaTipoRemocao(direita));
+				return true;
+			}
+		} else {
+			Node esquerda = raiz.getEsquerda();
+			if (esquerda == null)
+				return false;
+			else if (esquerda.getElemento() != elemento)
+				return remove(esquerda, elemento);
+			else {
+				raiz.setEsquerda(verificaTipoRemocao(esquerda));
+				return true;
+			}
+
+		}
+	}
+
+	/**
+	 * Metodo para verificar o tipo de remoção e executa-la
+	 * 
+	 * @param raiz
+	 * @return novo
+	 */
+	private Node verificaTipoRemocao(Node raiz) {
+		int qtd = raiz.getQuantidadeDeFilhos();
+		Node aux = raiz;
+		if (qtd == 0)
+			return null;
+		else if (qtd == 1)
+			return remocaoSimplesDireita(raiz);
+		else if (qtd == -1)
+			return remocaoSimplesEsquerda(raiz);
+		else {
+			return remocaoComposta(raiz);
+		}
+	}
+
+	/**
+	 * Caso o Node possua dois filhos
+	 * 
+	 * @param raiz
+	 * @return novo
+	 */
+	private Node remocaoComposta(Node raiz) {
+		Node aux;
+		aux = maiorEsquerda(raiz.getEsquerda());
+		aux.setDireita(raiz.getDireita());
+		if (raiz.getEsquerda() != aux)
+			aux.setEsquerda(raiz.getEsquerda());
+		return aux;
+	}
+
+	/**
+	 * Remoção simples, retorna o elemento a esquerda
+	 * 
+	 * @param raiz
+	 * @return novo
+	 */
+	private Node remocaoSimplesEsquerda(Node remove) {
+		return remove.getEsquerda();
+	}
+
+	/**
+	 * Remoção simples, retorna o elemento a direita
+	 * 
+	 * @param remove
+	 * @return novo
+	 */
+	private Node remocaoSimplesDireita(Node remove) {
+		return remove.getDireita();
+	}
+
+	/**
+	 * Retorna o maior elemento da sub árvore à esquerda
+	 * 
+	 * @param raiz
+	 * @return maiorEsquerda
+	 */
+	private Node maiorEsquerda(Node raiz) {
+
+		Node remover = raiz;
+
+		if (raiz.getDireita() != null)
+			remover = maiorEsquerda(raiz.getDireita());
+
+		if (raiz.getDireita() == remover) {
+			raiz.setDireita(remover.getEsquerda());
+		}
+
+		return remover;
+	}
+
+	/**
+	 * Retorna a raiz da árvore
+	 * 
+	 * @return raiz
+	 */
 	@Override
 	public Node getRaiz() {
 		return raiz;
 	}
 
-	@Override
-	public int getAltura() {
-		return 0;
+	/**
+	 * Percorre toda a árvore mostrando em preOrdem
+	 */
+	public void preOrdem() {
+		if (raiz == null)
+			System.out.println("Árvore vazia");
+		else
+			preOrdem(raiz);
+	}
+	/**
+	 * Percorre toda a árvore mostrando em emOrdem
+	 */
+	public void emOrdem() {
+		if (raiz == null)
+			System.out.println("Árvore vazia");
+		else
+			emOrdem(raiz);
+	}
+	/**
+	 * Percorre toda a árvore mostrando em posOrdem
+	 */
+	public void posOrdem() {
+		if (raiz == null)
+			System.out.println("Árvore vazia");
+		else
+			posOrdem(raiz);
+	}
+
+	/**
+	 * Metodo recursivo para percorrer toda a árvore
+	 * @param raiz
+	 */
+	private void preOrdem(Node raiz) {
+		if (raiz == null)
+			return;
+		System.out.println(raiz);
+		preOrdem(raiz.getEsquerda());
+		preOrdem(raiz.getDireita());
+
+	}
+	/**
+	 * Metodo recursivo para percorrer toda a árvore
+	 * @param raiz
+	 */
+	private void emOrdem(Node raiz) {
+		if (raiz == null)
+			return;
+		preOrdem(raiz.getEsquerda());
+		System.out.println(raiz);
+		preOrdem(raiz.getDireita());
+
+	}
+	/**
+	 * Metodo recursivo para percorrer toda a árvore
+	 * @param raiz
+	 */
+	private void posOrdem(Node raiz) {
+		if (raiz == null)
+			return;
+		preOrdem(raiz.getEsquerda());
+		preOrdem(raiz.getDireita());
+		System.out.println(raiz);
+
 	}
 
 }
