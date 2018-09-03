501436361b0aa908067c4fb8d1a5d1ef4de29a7a
João Pedro Amoêdo <jp.advc@gmail.com>
2018-09-03 16:43:16 -0300

    Terminando os métodos da árvore Binária e adicionando comentários

--- sistema-eleitoral/src/pi/teste/Teste.java
+++ sistema-eleitoral/src/pi/teste/Teste.java
@@ -1,12 +1,25 @@
 package pi.teste;
 
 import pi.arvore.ArvoreBinaria;
+import pi.node.Node;
 
 public class Teste {
 	public static void main(String[] args) {
 		
-		ArvoreBinaria ab = new ArvoreBinaria(55);
-		System.out.println("Testando tudo que fazer");
-		System.out.println("Ola Jeff");
+		ArvoreBinaria ab = new ArvoreBinaria(100);
+
+		ab.add(50);
+		ab.add(40);
+		ab.add(60);
+		ab.add(55);
+		ab.add(150);
+		
+		ab.remove(100);
+		
+		ab.emOrdem();
+
+
+		
+		
 	}
 }
